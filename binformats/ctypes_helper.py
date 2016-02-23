9ec3e79354c44fb846cec632afd22ed49afd0474
Sascha Schirra <sashs@scoding.de>
2016-02-23 23:31:14 +0100

    to_ubyte_array added

--- binformats/ctypes_helper.py
+++ binformats/ctypes_helper.py
@@ -16,6 +16,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+from struct import pack_into
 import ctypes
 
 def get_ptr(data, offset=None, ptr_type=ctypes.c_void_p):
@@ -29,3 +30,11 @@ def get_ptr(data, offset=None, ptr_type=ctypes.c_void_p):
         ptr = ctypes.cast(ptr, ptr_type)
 
     return ptr
+
+def to_ubyte_array(barray):
+    """Returns a c_ubyte_array filled with the given data of a bytearray or bytes"""
+    bs = (ctypes.c_ubyte * len(barray))()
+    pack_into('%ds' % len(barray), bs, 0, barray)
+
+    return bs
+
