1bf74cca6b2a155e27641b92d8163ead39172219
Sascha Schirra <sashs@scoding.de>
2016-02-23 23:33:42 +0100

    libc and new tests added

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 
 from binformats import elf
 
-efile = elf.ELF('test-binaries/ls-x86')
+efile = elf.ELF('test-binaries/libc-2.19.so')
 print("Segments:")
 for segment in efile.segments:
     print(segment.type, hex(segment.vaddr))
@@ -12,6 +12,9 @@ print('Sections:')
 for section in efile.sections:
     print(section.name, elf.SHT(section.header.sh_type).name)
 
+
 print()
 print('Symbols:')
-print(efile.symbols)
+for section in efile.sections:
+    if elf.SHT(section.header.sh_type) in (elf.SHT.DYNSYM, elf.SHT.SYMTAB):
+        print(section.name, ', '.join([sym.name+' '+elf.STT(sym.type).name+' '+hex(sym.header.st_value)+' '+hex(sym.header.st_value + sym.header.st_size) for sym in section.symbols if sym.header.st_shndx == 12]))
