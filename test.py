b74404240bf5034540fb38f7f9094a23ae5f6385
Sascha Schirra <sashs@scoding.de>
2016-02-23 15:17:10 +0100

    tests added

--- test.py
+++ test.py
@@ -3,5 +3,15 @@
 from binformats import elf
 
 efile = elf.ELF('test-binaries/ls-x86')
+print("Segments:")
 for segment in efile.segments:
     print(segment.type, hex(segment.vaddr))
+
+print()
+print('Sections:')
+for section in efile.sections:
+    print(section.name, elf.SHT(section.header.sh_type).name)
+
+print()
+print('Symbols:')
+print(efile.symbols)
