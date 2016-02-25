d9b3fa47d1213fa557dc80f0fc77e5ebbd23c23e
Sascha Schirra <sashs@scoding.de>
2016-02-25 23:00:23 +0100

    mach_o refactored

--- binformats/mach_o.py
+++ binformats/mach_o.py
@@ -1,61 +1,358 @@
 # coding=utf-8
 #
-# Copyright 2014 Sascha Schirra
+# Copyright 2016 Sascha Schirra
 #
-# This file is part of Ropper.
+# This file is part of filebytes.
 #
-# Ropper is free software: you can redistribute it and/or modify
+# filebytes is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Ropper is distributed in the hope that it will be useful,
+# filebytes is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-from ropper.loaders.loader import *
-from ropper.loaders.mach_intern.mach_gen import *
 from struct import pack as p
-import importlib
+from .enum import Enum
+from .binary import *
+############# MachO General ######################
 
-class SegmentData(DataContainer):
+class VM_PROT(Enum):
+    READ = 0x1
+    WRITE = 0x2
+    EXEC = 0x4
+
+    def shortString(self, perm):
+        toReturn = ''
+        toReturn += 'R' if perm & int(self.READ) > 0 else ' '
+        toReturn += 'W' if perm & int(self.WRITE) > 0 else ' '
+        toReturn += 'E' if perm & int(self.EXEC) > 0 else ' '
+
+        return toReturn
+
+
+class TypeFlags(Enum):
+    MASK = 0xff000000
+    ABI64 = 0x01000000
+
+
+class CpuType(Enum):
+    ANY = -1
+    I386 = 7
+    X86_64 = I386 | TypeFlags.ABI64
+    MIPS = 8
+    ARM = 12
+    ARM64 = ARM | TypeFlags.ABI64
+    SPARC = 14
+    POWERPC = 18
+    POWERPC64 = POWERPC | TypeFlags.ABI64
+    LC_SEGMENT = 1
+    LC_SEMGENT_64 = 0x19
+    S_ATTR_SOME_INSTRUCTIONS = 0x400
+    S_ATTR_PURE_INSTRUCTIONS = 0x80000000
+
+
+class SubTypeFlags(Enum):
+    MASK = 0xff000000
+    LIB64 = 0x80000000
+
+
+class CPU_SUBTYPE_X86(Enum):
+    X86 = 3
+    X86_64 = X86 | SubTypeFlags.LIB64
+    X86_64_H = 8
+    I486 = 4
+    I486SX = 0x84
+    I586 = 5
+    PENTPRO = 0x16
+    PENTII_M3 = 0x36
+    PENTII_M5 = 0x56
+    CELERON = 0x67
+    CELERON_MOBILE = 0x77
+    PENTIUM_3_M = 0x18
+    PENTIUM_3_XEON = 0x28
+    PENTIUM_M = 0x09
+    PENTIUM_4 = 0x0a
+    PENTIUM_4_M = 0x1a
+    ITANIUM = 0x0b
+    ITANIUM_2 = 0x1b
+    XEON = 0x0c
+    XEON_MP = 0x1c
+
+
+class LC(Enum):
+    SEGMENT = 0x00000001
+    SYMTAB = 0x00000002
+    SYMSEG = 0x00000003
+    THREAD = 0x00000004
+    UNIXTHREAD = 0x00000005
+    LOADFVMLIB = 0x00000006
+    IDFVMLIB = 0x00000007
+    IDENT = 0x00000008
+    FVMFILE = 0x00000009
+    PREPAGE = 0x0000000A
+    DYSYMTAB = 0x0000000B
+    LOAD_DYLIB = 0x0000000C
+    ID_DYLIB = 0x0000000D
+    LOAD_DYLINKER = 0x0000000E
+    ID_DYLINKER = 0x0000000F
+    PREBOUND_DYLIB = 0x00000010
+    ROUTINES = 0x00000011
+    SUB_FRAMEWORK = 0x00000012
+    SUB_UMBRELLA = 0x00000013
+    SUB_CLIENT = 0x00000014
+    SUB_LIBRARY = 0x00000015
+    TWOLEVEL_HINTS = 0x00000016
+    PREBIND_CKSUM = 0x00000017
+    LOAD_WEAK_DYLIB = 0x80000018
+    SEGMENT_64 = 0x00000019
+    ROUTINES_64 = 0x0000001A
+    UUID = 0x0000001B
+    RPATH = 0x8000001C
+    CODE_SIGNATURE = 0x0000001D
+    SEGMENT_SPLIT_INFO = 0x0000001E
+    REEXPORT_DYLIB = 0x8000001F
+    LAZY_LOAD_DYLIB = 0x00000020
+    ENCRYPTION_INFO = 0x00000021
+    DYLD_INFO = 0x00000022
+    DYLD_INFO_ONLY = 0x80000022
+    LOAD_UPWARD_DYLIB = 0x80000023
+    VERSION_MIN_MACOSX = 0x00000024
+    VERSION_MIN_IPHONEOS = 0x00000025
+    FUNCTION_STARTS = 0x00000026
+    DYLD_ENVIRONMENT = 0x00000027
+    MAIN = 0x80000028
+    DATA_IN_CODE = 0x00000029
+    SOURCE_VERSION = 0x0000002A
+    DYLIB_CODE_SIGN_DRS = 0x0000002B
+    LINKER_OPTIONS = 0x0000002D
+    LINKER_OPTIMIZATION_HINT = 0x0000002E
+
+
+class S_ATTR(Enum):
+    SOME_INSTRUCTIONS = 0x00000400
+    PURE_INSTRUCTIONS = 0x80000000
+
+
+class LoadCommand(LittleEndianStructure):
+    _fields_ = [('cmd', c_uint),
+                ('cmdsize', c_uint)]
+
+
+class UuidCommand(LittleEndianStructure):
+    _fields_ = [('cmd', c_uint),
+                ('cmdsize', c_uint),
+                ('uuid', c_ubyte * 16)]
+
+class TwoLevelHintsCommand(LittleEndianStructure):
+    _fields_ = [('cmd', c_uint),
+                ('cmdsize', c_uint),
+                ('offset', c_uint),
+                ('nhints', c_uint)]
+
+class TwoLevelHint(LittleEndianStructure):
+    _fields_ = [('isub_image', c_uint),
+                ('itoc', c_uint)]
+
+class LcStr(Union):
+    _fields_ = [('offset', c_uint)]
+
+
+########################### 32 BIT Structures ###########################
+
+class LSB_32_MachHeader(LittleEndianStructure):
+    _fields_ = [('magic', c_uint),
+                ('cputype', c_uint),
+                ('cpusubtype', c_uint),
+                ('filetype', c_uint),
+                ('ncmds', c_uint),
+                ('sizeofcmds', c_uint),
+                ('flags', c_uint)
+                ]
+
+
+class LSB_32_SegmentCommand(LittleEndianStructure):
+    _fields_ = [('cmd', c_uint),
+                ('cmdsize', c_uint),
+                ('segname', c_char * 16),
+                ('vmaddr', c_uint),
+                ('vmsize', c_uint),
+                ('fileoff', c_uint),
+                ('filesize', c_uint),
+                ('maxprot', c_uint),
+                ('initprot', c_uint),
+                ('nsects', c_uint),
+                ('flags', c_uint)]
+
+
+class LSB_32_Section(LittleEndianStructure):
+    _fields_ = [('sectname', c_char * 16),
+                ('segname', c_char * 16),
+                ('addr', c_uint),
+                ('size', c_uint),
+                ('offset', c_uint),
+                ('align', c_uint),
+                ('reloff', c_uint),
+                ('nreloc', c_uint),
+                ('flags', c_uint),
+                ('reserved1', c_uint),
+                ('reserved2', c_uint)
+                ]
+
+class LSB_32(object):
+    Section = LSB_32_Section
+    SegmentCommand = LSB_32_SegmentCommand
+    MachHeader = LSB_32_MachHeader
+
+########################### 64 BIT Structures ###########################
+
+class LSB_64_MachHeader(LittleEndianStructure):
+    _fields_ = [('magic', c_uint),
+                ('cputype', c_uint),
+                ('cpusubtype', c_uint),
+                ('filetype', c_uint),
+                ('ncmds', c_uint),
+                ('sizeofcmds', c_uint),
+                ('flags', c_uint),
+                ('reserved', c_uint),
+                ]
+
+
+class LSB_64_SegmentCommand(LittleEndianStructure):
+    _fields_ = [('cmd', c_uint),
+                ('cmdsize', c_uint),
+                ('segname', c_char * 16),
+                ('vmaddr', c_ulonglong),
+                ('vmsize', c_ulonglong),
+                ('fileoff', c_ulonglong),
+                ('filesize', c_ulonglong),
+                ('maxprot', c_uint),
+                ('initprot', c_uint),
+                ('nsects', c_uint),
+                ('flags', c_uint)]
+
+
+class LSB_64_Section(LittleEndianStructure):
+    _fields_ = [('sectname', c_char * 16),
+                ('segname', c_char * 16),
+                ('addr', c_ulonglong),
+                ('size', c_ulonglong),
+                ('offset', c_uint),
+                ('align', c_uint),
+                ('reloff', c_uint),
+                ('nreloc', c_uint),
+                ('flags', c_uint),
+                ('reserved1', c_uint),
+                ('reserved2', c_uint)
+    ]
+
+class LSB_64(object):
+    Section = LSB_64_Section
+    SegmentCommand = LSB_64_SegmentCommand
+    MachHeader = LSB_64_MachHeader
+
+
+############################### Container #############################
+
+
+class MachHeaderData(Container):
     """
-    struct = SegmentCommand
-    name = string (section name)
-    bytes = c_byte_array (section bytes)
+    header = MachHeader
     """
 
-class LoaderData(DataContainer):
+class LoadCommandData(Container):
     """
-    struct = LoaderCommand
-
-    """
-
-class SectionData(DataContainer):
-    """
-    struct = Section
+    header = LoaderCommand
 
     """
 
-class MachO(Loader):
+class SectionData(Container):
+    """
+    header = Section
 
-    def __init__(self, filename):
+    """
 
-        self.loaderCommands = []
-        self.header = None
-        self.segments = []
-        self.__module = None
-        self.__imageBase = None
+class MachO(Binary):
 
-        super(MachO, self).__init__(filename)
+    def __init__(self, fileName, fileContent=None):
+        super(MachO, self).__init__(fileName, fileContent)
+
+        self.__classes = self._getSuitableClasses(self._bytes)
+        if not self.__classes:
+            raise BinaryError('Bad architecture')
+
+        self.__machHeader = self._parseMachHeader(self._bytes)
+        self.loadCommands = self._parseLoadCommands(self._bytes, self.machHeader)
+
+    @property
+    def _classes(self):
+        return self.__classes
+
+    @property
+    def machHeader(self):
+        return self.__machHeader
+    
 
     @property
     def entryPoint(self):
         return 0x0
 
+    @property
+    def type(self):
+        return 'MachO'
+
+    def _getSuitableClasses(self, data):
+        classes = None
+        if data[7] == 0:
+            classes = LSB_32
+        elif data[7] == 1:
+            classes = LSB_64
+        
+        return classes
+
+    def _parseMachHeader(self, data):
+        header = self._classes.MachHeader.from_buffer(data)
+
+        if header.magic not in (0xfeedface, 0xfeedfacf, 0xcefaedfe, 0xcffaedfe):
+            raise BinaryError('No valid MachO file')
+
+        return MachHeaderData(header=header)
+
+    def _parseLoadCommands(self, data, machHeader):
+        offset = sizeof(self._classes.MachHeader)
+        load_commands = []
+        for i in range(machHeader.header.ncmds):
+            command = LoadCommand.from_buffer(data, offset)
+
+            if command.cmd == LC.SEGMENT or command.cmd == LC.SEGMENT_64:
+                seg_command = self.__parseSegmentCommand(data, offset)
+                load_commands.append(seg_command)
+            else:
+                load_commands.append(LoadCommandData(header=command))
+
+            offset += command.cmdsize
+
+        return load_commands
+
+    def __parseSegmentCommand(self, data, offset):
+        sc = self._classes.SegmentCommand.from_buffer(data, offset)
+        sections = self.__parseSections(data, sc, offset+sizeof(self._classes.SegmentCommand))
+        return LoadCommandData(header=sc, name=str(sc.segname, 'ASCII'), sections=sections)
+
+    def __parseSections(self, data, segment, offset):
+        
+        sections = []
+        for i in range(segment.nsects):
+            sec = self._classes.Section.from_buffer(data, offset)
+
+            offset += sizeof(self._classes.Section)
+            sections.append(SectionData(header=sec))
+
+        return sections
     
     def _getImageBase(self):
         if self.__imageBase == None:
@@ -67,114 +364,9 @@ class MachO(Loader):
                 self.__imageBase = 0x0
         return self.__imageBase
 
-    
-    def _loadDefaultArch(self):
-        try:
-            return ARCH[self.header.cputype]
-        except:
-            return None
-    @property
-    def type(self):
-        return Type.MACH_O
-
-    @property
-    def executableSections(self):
-        toReturn = []
-        for loaderCommand in self.loaderCommands:
-            if loaderCommand.struct.cmd == LC.SEGMENT or loaderCommand.struct.cmd == LC.SEGMENT_64:
-                for section in loaderCommand.sections:
-                    section = section.struct
-                    if section.flags & S_ATTR.SOME_INSTRUCTIONS  or section.flags & S_ATTR.PURE_INSTRUCTIONS:
-                        sectbytes_p = c_void_p(self._bytes_p.value + section.offset)
-                        sectbytes = cast(sectbytes_p, POINTER(c_ubyte * section.size)).contents
-                        toReturn.append(Section(section.sectname, sectbytes, section.addr, section.offset))
-        return toReturn
-
-    @property
-    def dataSections(self):
-        toReturn = []
-        for loaderCommand in self.loaderCommands:
-            if loaderCommand.struct.cmd == LC.SEGMENT or loaderCommand.struct.cmd == LC.SEGMENT_64:
-                for section in loaderCommand.sections:
-                    section = section.struct
-                    if not section.flags & S_ATTR.SOME_INSTRUCTIONS  or  not section.flags & S_ATTR.PURE_INSTRUCTIONS:
-                        sectbytes_p = c_void_p(self._bytes_p.value + section.offset)
-                        sectbytes = cast(sectbytes_p, POINTER(c_ubyte * section.size)).contents
-                        toReturn.append(Section(section.sectname, sectbytes, section.addr, section.offset))
-        return toReturn
-
-    def getSection(self, name):
-        for loaderCommand in self.loaderCommands:
-            if loaderCommand.struct.cmd == LC.SEGMENT or loaderCommand.struct.cmd == LC.SEGMENT_64:
-                for section in loaderCommand.sections:
-                    section = section.struct
-                    if str(section.sectname) == name:
-                        sectbytes_p = c_void_p(self._bytes_p.value + section.offset)
-                        sectbytes = cast(sectbytes_p, POINTER(c_ubyte * section.size)).contents
-                        return Section(section.sectname, sectbytes, section.addr, section.offset)
-        raise RopperError('No such secion: %s' % name)        
-        
-
-    def __loadModule(self):
-        modName = None
-        if self._bytes[7] == 0:
-            modName = 'ropper.loaders.mach_intern.mach32'
-        elif self._bytes[7] == 1:
-            modName = 'ropper.loaders.mach_intern.mach64'
-        else:
-            raise LoaderError('Bad architecture')
-        self.__module = importlib.import_module(modName)
-
-    def __parseSections(self, segment, segment_p):
-        p_tmp = c_void_p(segment_p.value + sizeof(self.__module.SegmentCommand))
-        sections = []
-        for i in range(segment.nsects):
-            sec = cast(p_tmp, POINTER(self.__module.Section)).contents
-
-
-            p_tmp.value += sizeof(self.__module.Section)
-            sections.append(SectionData(struct=sec))
-
-        return sections
-
-    def __parseSegmentCommand(self, segment_p):
-        sc = cast(segment_p, POINTER(self.__module.SegmentCommand)).contents
-        sections = self.__parseSections(sc, segment_p)
-        return SegmentData(struct=sc, name=sc.segname, sections=sections)
-
-    def __parseCommands(self):
-        p_tmp = c_void_p(self._bytes_p.value + sizeof(self.__module.MachHeader))
-        for i in range(self.header.ncmds):
-            command = cast(p_tmp, POINTER(LoadCommand)).contents
-            if command.cmd == LC.SEGMENT or command.cmd == LC.SEGMENT_64:
-                self.loaderCommands.append(self.__parseSegmentCommand(p_tmp))
-            else:
-                self.loaderCommands.append(LoaderData(struct=command))
-            p_tmp.value += command.cmdsize
-
-    def __parseHeader(self):
-        self.header = cast(self._bytes_p, POINTER(self.__module.MachHeader)).contents
-
-    def _parseFile(self):
-        self.__loadModule()
-        self.__parseHeader()
-        self.__parseCommands()
-
-    def setNX(self, enable):
-        raise LoaderError('Not available for mach-o files')
-
-    def setASLR(self, enable):
-        raise LoaderError('Not available for mach-o files')
-
-
-    def checksec(self):
-        return {}
-
     @classmethod
-    def isSupportedFile(cls, fileName):
-        try:
-            with open(fileName, 'rb') as f:
-                magic = f.read(4)
-                return magic == p('>I', 0xfeedface) or magic == p('>I', 0xfeedfacf) or magic == p('<I', 0xfeedface) or magic == p('<I', 0xfeedfacf)
-        except BaseException as e:
-            raise LoaderError(e)
+    def isSupportedContent(cls, fileContent):
+        """Returns if the files are valid for this filetype"""
+        magic = bytearray(fileContent)[:4]
+        return magic == p('>I', 0xfeedface) or magic == p('>I', 0xfeedfacf) or magic == p('<I', 0xfeedface) or magic == p('<I', 0xfeedfacf)
+
