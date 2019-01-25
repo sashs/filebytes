004cc90b301c2f3de1679bd8324590b1fd70f0c8
KOLANICH <KOLANICH@users.noreply.github.com>
2019-01-25 12:13:08 +0000

    Removed everything unneeded from setup.py

--- setup.py
+++ setup.py
@@ -1,33 +1,17 @@
-from setuptools import setup, find_packages
-import filebytes
-version = filebytes.VERSION
-package_name = "filebytes"
-package_dir = "filebytes"
-package_description = """
-Scripts to parse the following file formats - Executable and Linking Format (ELF), Portable Executable (PE), Mach-O and OAT (Android Runtime).
-""".strip()
+from setuptools import setup
+import ast
+from pathlib import Path
 
-packages = find_packages()
-valid_packages = []
-for p in packages:
-    if p.startswith('filebytes'):
-        valid_packages.append(p)
+currentDir = Path(__file__).parent
 
-setup(
-    name=package_name,
-    version=version,
-    description=package_description,
-    packages=valid_packages,
-    license="BSD",
-    author="Sascha Schirra",
-    author_email="sashs@scoding.de",
-    url="https://scoding.de/filebytes-introduction/",
-    classifiers=[
-        'Topic :: Security',
-        'Environment :: Console',
-        'Operating System :: OS Independent',
-        'License :: OSI Approved :: BSD 3 Clause License',
-        'Programming Language :: Python',
-        'Intended Audience :: Developers'
-    ]
-)
+def extractMetaInfo(src):
+    info = {}
+    a=ast.parse(src)
+    for e in a.body:
+        if isinstance(e, ast.Assign) and isinstance(e.value, ast.Str):
+            info[e.targets[0].id] = e.value.s
+    return info
+
+version = extractMetaInfo((currentDir / "filebytes" / "__init__.py").read_text())["VERSION"]
+
+setup(version=version)
